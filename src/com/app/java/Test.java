299412e186eddf82cdbbd9225dded367399fb4e1
anudeep5e7 <anudeeprachamalla@gmail.com>
2022-09-07 22:45:30 +0530

    bajajF

---
+++ src/com/app/java/Test.java
@@ -0,0 +1,15 @@
+package com.app.java;
+
+public class Test {
+
+	public void showMsg(String s)
+	{
+		System.out.println(s);
+	}
+	public static void main(String[] args) {
+		
+		Test t=new Test();
+		
+		t.showMsg("welcome to java");
+	}
+}
